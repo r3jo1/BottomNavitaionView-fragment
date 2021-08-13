f05f4e4ae2bc43a7d46db9adf29803f95311dac4
K M Rejowan Ahmmed <ahmmedrejowan@gmail.com>
2021-08-13 15:29:07 +0600

    Changes in UI

--- app/src/main/java/com/rejowan/bottomnav/FavouriteFragment.java
+++ app/src/main/java/com/rejowan/bottomnav/FavouriteFragment.java
@@ -1,12 +1,11 @@
 package com.rejowan.bottomnav;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 public class FavouriteFragment extends Fragment {
 
--- app/src/main/java/com/rejowan/bottomnav/Home.java
+++ app/src/main/java/com/rejowan/bottomnav/Home.java
@@ -1,16 +1,39 @@
 package com.rejowan.bottomnav;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.rejowan.bottomnav.databinding.ActivityHomeBinding;
 
 public class Home extends AppCompatActivity {
 
+    ActivityHomeBinding activityHomeBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+
+        // View Binding
+        activityHomeBinding = ActivityHomeBinding.inflate(getLayoutInflater());
+        View view = activityHomeBinding.getRoot();
+        setContentView(view);
+
+        // App Bar for Setting the titles
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.homeFragment, R.id.galleryFragment, R.id.favouriteFragment
+        ).build();
+
+
+        // Navigation Controller
+        NavController navController = Navigation.findNavController(this, R.id.fragment_container);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration); // for changing the title
+        NavigationUI.setupWithNavController(activityHomeBinding.bottomNav, navController);
 
 
     }
