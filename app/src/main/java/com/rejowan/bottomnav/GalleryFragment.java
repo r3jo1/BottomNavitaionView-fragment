eabe383dde6eb78b2c2bf0577b37c2449d096056
K M Rejowan Ahmmed <ahmmedrejowan@gmail.com>
2021-08-13 15:15:58 +0600

    Changes in UI

---
+++ app/src/main/java/com/rejowan/bottomnav/GalleryFragment.java
@@ -0,0 +1,30 @@
+package com.rejowan.bottomnav;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class GalleryFragment extends Fragment {
+
+
+    public GalleryFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_gallery, container, false);
+    }
+}
---
+++ app/src/main/java/com/rejowan/bottomnav/Home.java
@@ -0,0 +1,18 @@
+package com.rejowan.bottomnav;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+    }
+
+}
